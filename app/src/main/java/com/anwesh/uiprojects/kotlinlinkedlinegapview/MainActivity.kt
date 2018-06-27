97752976c3ad5affd6ab0f5b1a52cf52ea3cffda
Anwesh43 <anweshthecool0@gmail.com>
2018-06-27 09:27:15 +0530

    adding view in activity

--- app/src/main/java/com/anwesh/uiprojects/kotlinlinkedlinegapview/MainActivity.kt
+++ app/src/main/java/com/anwesh/uiprojects/kotlinlinkedlinegapview/MainActivity.kt
@@ -2,10 +2,12 @@ package com.anwesh.uiprojects.kotlinlinkedlinegapview
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import com.anwesh.uiprojects.linkedlinegapview.LinkedLineGapView
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        LinkedLineGapView.create(this)
     }
 }
